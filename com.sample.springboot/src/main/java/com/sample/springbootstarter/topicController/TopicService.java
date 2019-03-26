7a0970d058566d702766576c452c1125bfd2c77f
Prakash Singh <48259170+prakashkrsingh@users.noreply.github.com>
2019-03-27 01:42:01 +0530

    Sample Spring Boot example with java 8

---
+++ com.sample.springboot/src/main/java/com/sample/springbootstarter/topicController/TopicService.java
@@ -0,0 +1,55 @@
+package com.sample.springbootstarter.topicController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	@Autowired
+	private TopicRepository topicRepository;
+	
+//	private List<Topic> topics=new ArrayList<>(Arrays.asList(
+//			
+//			new Topic("java" , "Core Java", "java description"),
+//			new Topic("Spring" , "Spring framework", "Spring description"),
+//			new Topic("Spring Boot" , "Spring boot", "Spring Boot Description")
+//			
+//			));
+
+	public List<Topic> getAllTopics(){
+		//return topics;
+		List<Topic> topics = new ArrayList<>();
+		topicRepository.findAll().forEach(topics::add);
+		return topics;
+	}
+	
+	public Topic getTopic(String id){	
+		
+		//return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+		return topicRepository.findOne(id);
+		
+	}
+
+	public void addTopic(Topic topic) {
+		//topics.add(topic);
+		topicRepository.save(topic);
+		
+	}
+
+	public void updateTopic(Topic topic, String id) {
+		
+		topicRepository.save(topic);
+	}
+
+	public void deleteTopic(Topic topic, String id) {
+		
+	//	topics.removeIf(t -> t.getId().equals(id));
+		topicRepository.delete(id);
+	}
+	
+}
